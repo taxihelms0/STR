3d69192e28f0b6033d298f084da21d5a2a429d09
Alexander Smith <alex@Alexanders-MacBook-Air.local>
2019-11-10 13:23:13 -0600

    commented everything

--- str_nowave.py
+++ str_nowave.py
@@ -2,7 +2,6 @@
 # Alex Ian Smith 2019
 
 import sys
-# from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import (QApplication, QPushButton, QWidget,
 QInputDialog, QLineEdit, QFileDialog, QSlider, QDialog, QVBoxLayout, QLabel,
 QGridLayout, QStatusBar, QToolButton, QHBoxLayout, QStyleFactory, QMainWindow,
@@ -13,45 +12,37 @@ from PyQt5.QtCore import QThread, pyqtSignal
 import math
 import random
 from pathlib import Path
-# import sounddevice as sd
-# import soundfile as sf
 import threading
 import os
 import ntpath
 from stretch import stretch0
-from scipy.io.wavfile import write
 import numpy
 import webbrowser
 import time
 import wavio
 
-# future stuff:
-# 1. create dicts for buttons/frames, etc, so all you have to do is iterate
-# through the list to load everything (less copy/paste)
-#
-# 2. create one layout template that gets reused, but changes the names/values of
-# buttons based on which tab is clicked
-
 class Process_function(QThread):
+    # class var for stretch Results
     stretch_results = pyqtSignal(numpy.ndarray, int)
-    #processes the things
+
     def doit(self, data, fs, factor_value, buffer_value, offset_value,
     start_value, end_value, max_value):
         app.processEvents()
+        #process the loaded wave data
         write_data, write_fs = stretch0(data, fs,
         factor_value, buffer_value, offset_value,
         start_value, end_value, max_value)
-
-        # App().onButtonClick(f"{len(write_data)} samples at {fs}")
+        # send results to 'receiveResults'
         self.stretch_results.emit(write_data, write_fs)
 
 class External(QThread):
-    """
-    Runs a counter thread.
-    """
+    # threaded processing for status text readout
+    # I don't totally understand how this works yet.
+    # class var
     messageChanged = pyqtSignal(str)
 
     def status(self, message):
+        # function that actually updates the status
         self.messageChanged.emit(message)
 
 class App(QWidget):
@@ -70,22 +61,25 @@ class App(QWidget):
         self.update()
 
         self.click = Process_function()
-        self.click.stretch_results.connect(self.recieveResults)
+        self.click.stretch_results.connect(self.receiveResults)
         self.click.doit(self.data, self.fs, self.factor_value,
         self.buffer_value, self.offset_value, self.start_value, self.end_value,
         self.max_value)
 
-    def recieveResults(self, data, fs):
+    def receiveResults(self, data, fs):
+        # receives numpy array after being processed by stretch0
         if len(data) == 0:
-            self.onButtonClick('Unknown error processing. Try again with different parameters')
+            # if no data received
+            self.onButtonClick('Error: Write Buffer Empty. Try again with different parameters')
         else:
-            self.onButtonClick('')
-            self.write_data = data
-            self.write_fs = fs
-            self.saveFileDialog()
-#########################################
+            # if data received
+            self.onButtonClick('')  # send which button clicked to status readout
+            self.write_data = data  # assign data
+            self.write_fs = fs      # assign rate
+            self.saveFileDialog()   # call savefile dialog func
 
     def __init__(self):
+        # initialize
         super().__init__()
         self.title = 'STR'
         self.left = 200
@@ -100,8 +94,7 @@ class App(QWidget):
         self.update()
 
     def initval(self):
-        # stretch0(self.data, self.fs, self.factor_value, self.buffer_value,
-        # self.offset_value, self.start_value, self.end_value, self.max_value)
+        # set initial variable values on startup
         self.data = 0
         self.fs = 0
         self.factor_value = 1
@@ -112,30 +105,22 @@ class App(QWidget):
         self.max_value = 0.0165
         self.filename = 0
 
-    # def initstatus(self):
-    #     self.statusBar = QStatusBar()
-    #     self.setStatusBar(self.statusBar)
-    #     self.statusbar().showMessage("Statusbar")
-
     def initUI(self):
+        # initialize UI
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.layout = QGridLayout()     # establish layout
+        self.layout.setSpacing(10)      # set global spacing
 
-        # self.setWindowIcon(QIcon('sine.png'))
-        self.layout = QGridLayout()
-        self.layout.setSpacing(10)
-        # self.setStyleSheet("QLabel, QPushButton {font: 14pt Courier}")
+        # add buttons to layout
 
-        self.filelabel = QLabel("\n\n")
         self.layout.addWidget(self.filelabel, 11, 0, 2, 2)
         self.layout.addWidget(self.openbutton, 12, 2)
-        # self.layout.addWidget(self.playbutton, 12, 3)
         self.layout.addWidget(self.randombutton, 12, 6)
         self.layout.addWidget(self.processbutton, 12, 7)
-        # self.layout.addWidget(self.previewbutton, 12, 8)
-        # self.layout.addWidget(self.savebutton, 12, 10)
         self.layout.addWidget(self.aboutbutton, 12, 11)
 
+        # add labels to Layout
         self.layout.addWidget(self.factorlabel, 0, 0, 1, 2)
         self.layout.addWidget(self.factorslider, 0, 2, 1, 10)
         self.layout.addWidget(self.buflabel, 2, 0)
@@ -150,6 +135,8 @@ class App(QWidget):
         self.layout.addWidget(self.maxslidermin, 10, 2, 1, 10)
         self.layout.addWidget(self.maxslidersec, 11, 2, 1, 10)
 
+        # add sliders to layout
+
         self.statuslabel = QLabel('')
         self.layout.addWidget(self.statuslabel, 13, 2, 1, 10)
 
@@ -159,18 +146,11 @@ class App(QWidget):
         self.show()
 
     def initbuttons(self):
-        # buttons
+        # initialize buttons
         # open
         self.openbutton = QPushButton("Open", self)
         self.openbutton.setToolTip('Open a Wav file')
         self.openbutton.clicked.connect(self.openFileNameDialog)
-
-        # play
-        # self.playbutton = QPushButton("Play", self)
-        # # self.playbutton.setText('Play')
-        # # self.playbutton.setChecked(False)
-        # self.playbutton.setToolTip('play selected .wav file')
-        # self.playbutton.clicked.connect(self.play)
 
         # random
         self.randombutton = QPushButton("Random", self)
@@ -182,22 +162,12 @@ class App(QWidget):
         self.processbutton.setToolTip('Processes Wav file')
         self.processbutton.clicked.connect(self.process)
 
-        # preview
-        # self.previewbutton = QPushButton("Preview", self)
-        # self.previewbutton.setToolTip('Preview Processed Results')
-        # self.previewbutton.clicked.connect(self.preview)
-
-        # save
-        # self.savebutton = QPushButton("Save", self)
-        # self.savebutton.setToolTip('Saves to a new Wav file')
-        # self.savebutton.clicked.connect(self.saveFileDialog)
-
         # about
         self.aboutbutton = QPushButton("About", self)
         self.aboutbutton.clicked.connect(self.aboutdialog)
 
     def initsliders(self):
-        # Sliders
+        # initialize Sliders and labels
         # factor
         self.factorslider = QSlider(Qt.Horizontal)
         self.factorslider.setMinimum(-100)
@@ -269,97 +239,74 @@ class App(QWidget):
         self.maxlabel.setFixedWidth(self.colwidth)
         self.maxlabel.setAlignment(Qt.AlignRight)
 
+        # loaded filename display
+        self.filelabel = QLabel("\n\n")
+
     def open_webbrowser(self):
+        # func to open website
         webbrowser.open('http://alexiansmith.com')
 
     def factor(self):
+        # func for factor slider
         self.update()
         self.factor_value = self.factorslider.value()
         self.factorlabel.setText('Factor:\n' + str(self.factor_value))
 
     def buf(self):
+        # func for buffer slider
         self.update()
         self.buffer_value = float(self.bufslider.value()/1000)
         self.buflabel.setText('Buffer:\n' + str(self.buffer_value))
 
     def offset(self):
+        # func for offset slider
         self.offset_value = float(self.offsetslider.value()/1000)
         self.offsetlabel.setText('Offset:\n' + str(self.offset_value))
 
     def start(self):
+        # func for start slider
         self.start_value = self.startslider.value()
         self.startlabel.setText('Start:\n' + str(self.start_value))
 
     def end(self):
+        # func for end slider
         self.end_value = self.endslider.value()
         self.endlabel.setText('End:\n' + str(self.end_value))
 
     def max(self):
+        # func for max sliders
         min = int(self.maxslidermin.value())
         sec = float((self.maxslidersec.value())/100)
         self.max_value = (min + sec/60)
-        # pass # print(self.max_value)
         self.maxlabel.setText(f"Max Length:\n{str(min)} min {str(sec)} sec")
         if self.max_value > 1.5:
+            # warning about longer outputs
             self.onButtonClick("Careful! more than a couple minutes can take A LOT of time and computer resources to process")
         else:
             self.onButtonClick('')
 
     def open(self):
-
-
+        # loads wave file to memory
         try:
             wavein = wavio.read(self.filename)
             self.data = wavein.data
             self.fs = wavein.rate
-            print(self.data)
-            print(self.fs)
-            # self.data, self.fs = sf.read(self.filename, dtype='int16')
+            self.swidth = wavein.sampwidth
+            # updates file label
             self.filelabel.setText("File Loaded:\n" + ntpath.basename(self.filename))
             self.filelabel.setAlignment(Qt.AlignRight)
         except:
             self.onButtonClick("Couldn't load wav file")
 
-    def play(self):
-        # pass # print(self.playbutton.isChecked())
-
-        if self.filename != 0:
-            if self.playbutton.text() == 'Stop':
-                try:
-                    self.onButtonClick('')
-                    self.timer.cancel()
-                except:
-                    pass # print("couldn't stop timer")
-                self.playbutton.setText('Play')
-                sd.stop()
-                # self.playbutton.toggle()
-            else:
-                try:
-                    self.onButtonClick('Playing...')
-                    l = (len(self.data) / self.fs)
-                    # pass # print(f"{l} seconds")
-                    sd.play(self.data, self.fs)
-                    self.playbutton.setText('Stop')
-                    self.timer = threading.Timer(l, self.revert_playbutton)
-                    self.timer.start()
-                except:
-                    self.onButtonClick("couldn't play")
-
-        else:
-            self.onButtonClick("Nothing to play. Open a file first")
-
-    def revert_playbutton(self):
-        self.onButtonClick('')
-        self.playbutton.setText('Play')
-
     def randomize(self):
+        # randomizes values
         self.update()
         self.f = random.uniform(-50, 50)
         self.b = random.uniform(0, 49000)
         self.o = random.uniform(0, 1000)
         self.s = random.uniform(0, 60)
-        self.e = random.uniform(0, 99)
         self.update()
+        # applies values to slider display
         self.factorslider.setValue(self.f)
         self.bufslider.setValue(self.b)
         self.offsetslider.setValue(self.o)
@@ -368,127 +315,91 @@ class App(QWidget):
         self.endslider.setValue(self.e)
         self.update()
 
-    def preview(self):
-
-        if self.previewbutton.text() == 'Stop':
-            try:
-                self.onButtonClick('')
-                self.ptimer.cancel()
-            except:
-                pass # print("couldn't stop timer")
-            # self.previewbutton.setText('Preview')
-            sd.stop()
-            # self.playbutton.toggle()
-        else:
-            try:
-                self.onButtonClick('Previewing...')
-                l = (len(self.write_data) / self.write_fs)
-                # pass # print(f"{l} seconds")
-                sd.play(self.write_data, self.write_fs)
-                # self.previewbutton.setText('Stop')
-                self.ptimer = threading.Timer(l, self.revert_previewbutton)
-                self.ptimer.start()
-            except:
-                self.onButtonClick("Nothing to preview. Process something first")
-
-    def revert_previewbutton(self):
-        self.onButtonClick('')
-        # self.previewbutton.setText('Preview')
-
-        # try:
-        #     sd.play(self.write_data, self.write_fs)
-        # except:
-        #     pass # print("couldn't preview")
-
     def process(self):
+        # Process button clicked
         if self.start_value > self.end_value:
-            # temp = []
-            # indatalen = len(self.data)
-            # for i in range(indatalen):
-            #     temp.append(self.data[indatalen - i - 1])
-            # newstart = 99 - self.start_value
-            # newend = 99 - self.end_value
-            # pass # print(f"newend {newend}, newstart {newstart}")
-            # start_value = newstart
-            # end_value = newend
             self.onButtonClick("Startpoint cannot be larger than Endpoint")
         else:
-
+            # update status label
             self.onButtonClick("Processing...   STR will be unresponsive until Process completes")
-            self.processClick()
+            self.processClick()     # call processClick function to process data
             self.update()
 
-
-
-
     def revert_processing(self):
+        # resets status label when processing is complete
         self.onButtonClick('')
 
     def save(self):
+        # saves processed data to a wave file
+
+
         try:
-            # save write_buffer to file
-            write(self.save_filename, self.write_fs, self.write_data)
+            wavio.write(self.save_filename, self.write_data, self.write_fs)
+            # status label readout
             self.onButtonClick(f"{ntpath.basename(self.save_filename)} saved")
         except:
             self.onButtonClick(f"save error: couldn't save {ntpath.basename(self.save_filename)}")
 
     def openFileNameDialog(self):
-        # options = QFileDialog.Options()
-        # options |= QFileDialog.DontUseNativeDialog
-        # file = inspect.getframeinfo(inspect.currentframe()).filename
-
-        home = str(Path.home())
-        # base_dir = os.path.dirname(os.path.abspath(__file__))
+        # open dialog
+        home = str(Path.home())     # not sure if this works...
+        # base_dir = os.path.dirname(os.path.abspath(__file__)) leaving this code in for future testing
+        # call dialog & set selected file to self.filename
         self.filename, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()","",home,"Wav Files (*.wav)")
         if self.filename:
-            pass # print(self.filename)
             self.open()
 
     def ReadMe(self):
+        # opens readme in a text editor
         os.system('open ReadMe.md')
 
     def aboutdialog(self):
+        # opens a new window for about section
         self.aboutwindow = QDialog()
-        # self.aboutwindow.setGeometry(self.left + 50, self.top + 50, 400, 300)
 
+        # define buttons
         b1 = QPushButton("alexiansmith.com",self.aboutwindow)
         b1.clicked.connect(self.open_webbrowser)
         b2 = QPushButton("Close")
         b2.clicked.connect(self.aboutwindow.close)
         b3 = QPushButton("ReadMe")
         b3.clicked.connect(self.ReadMe)
+        # fix this so it just reads the selected lines from the readme file
         l1 = QLabel("STR is an application for manipulating '.wav' files\nfor creative and sometimes unexpected results\n\n © Alex Ian Smith 2019\n\n")
 
+        # add buttons & labels to layout
         abt = QVBoxLayout()
         abt.addWidget(l1)
         abt.addStretch()
         abt.addWidget(b3)
         abt.addWidget(b1)
         abt.addWidget(b2)
+
+        # set layout
         self.aboutwindow.setLayout(abt)
         self.aboutwindow.setWindowTitle("About")
         self.aboutwindow.setWindowModality(Qt.ApplicationModal)
         self.aboutwindow.exec_()
 
     def saveFileDialog(self):
-        # try:
-        if self.write_fs:
-            # options = QFileDialog.Options()
-            # options |= QFileDialog.DontUseNativeDialog
-            home = str(Path.home())
-            self.save_filename, _ = QFileDialog.getSaveFileName(self,"Save","",home,"Wav Files (*.wav)")
-            if self.save_filename:
-                # pass # print(self.save_filename)
-                self.save()
-        # except:
-            # self.onButtonClick("Nothing to save. Process something first")
+        # opens save file dialog
+        try:
+            if self.write_fs:
+                # options = QFileDialog.Options()
+                # options |= QFileDialog.DontUseNativeDialog
+                home = str(Path.home())
+                self.save_filename, _ = QFileDialog.getSaveFileName(self,"Save","",home,"Wav Files (*.wav)")
+                if self.save_filename:
+                    # pass # print(self.save_filename)
+                    self.save()
+        except:
+            self.onButtonClick("Nothing to save. Process something first")
 
 if __name__ == '__main__':
-    # appctxt = ApplicationContext()
+    # it's main. the actual program loop
     app = QApplication(sys.argv)
     base_dir = os.path.dirname(os.path.abspath(__file__))
     app.setWindowIcon(QIcon(base_dir + '/STR-icon.png'))
     ex = App()
-    # exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     app.setStyle('Fusion')
     sys.exit(app.exec_())
